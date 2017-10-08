4c495b260b43aa907e574e6b6364009d9897a915
electronique <joe@joereinsel.org>
2017-10-08 10:43:00 -0400

    new changes

--- Week5/call_center/callcenter.py
+++ Week5/call_center/callcenter.py
@@ -1,24 +1,25 @@
 import datetime
 
 class call(object):
-    def __init__(self,unique_id,name,phone,reason):
+    def __init__(self,unique_id,name,phone,time,reason):
         self.unique_id = unique_id
         self.name = name
         self.phone = phone
-        time = datetime.time
-        self.time = time
         self.reason = reason
+        self.time = time
     def displayCall(self):
         print self.unique_id
         print self.name
         print self.phone
         print self.time
         print self.reason
-
-class callCenter(call):
     
-        print call
+class callCenter(call):
+    def __init__(self,list_calls,call_list_len):
+        self.list_calls = list_calls
+        self.call_list_len = call_list_len
+        return self
 
-newcall = callCenter(1,"Joe","443-932-5443","questions")
+newcall = callCenter(1,"Joe","443-932-5443","5:30","questions")
 
 newcall.displayCall()
---
+++ Week5/django/main/apps/blogs/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render,HttpResponse,redirect
+
+# Create your views here.
+
+def index(request):
+    response = "placeholder to later display all the list of blogs"
+    return HttpResponse(response)
+def new(request):
+    response = "placeholder to later display all the list of blogs"
+    return HttpResponse(response)
+def create(request):
+    return redirect('/')
+def blog_post(request,number):
+    response = "Placeholder for blog post"+str(number)
+    return HttpResponse(response)
+def blog_post_edit(request,number):
+    response = "Placeholder for edit blog post"+str(number)
+    return HttpResponse(response)
+def destroy(request,number):
+    return redirect('/')
