531d1efea5bc0c6f1fd2b09ea1b75395d1016056
electronique <joe@joereinsel.org>
2017-10-09 22:25:48 -0400

    new changes

---
+++ Week6/surveyform/main/apps/survey/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request,"survey/index.html")
+
+def result(request):
+    
+    request.session['name'] = request.POST['name']
+    request.session['location'] = request.POST['location']
+    request.session['favlang'] = request.POST['favlang']
+    request.session['comments'] = request.POST['comments']
+    return render(request,"survey/result.html")
