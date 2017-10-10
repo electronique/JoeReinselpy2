6da827044356f2936239ea7e4e5c2f2941ef85d8
electronique <joe@joereinsel.org>
2017-10-10 13:29:39 -0400

    new changes

--- Week6/surveyform/main/apps/survey/views.py
+++ Week6/surveyform/main/apps/survey/views.py
@@ -5,9 +5,29 @@ def index(request):
     return render(request,"survey/index.html")
 
 def result(request):
-    
+    try:
+        request.session['counter']
+    except(KeyError):
+        request.session['counter']=1
+
+    context = {
+        'counting': request.session['counter']
+    }
+    request.session['counter']+=1
+
     request.session['name'] = request.POST['name']
     request.session['location'] = request.POST['location']
     request.session['favlang'] = request.POST['favlang']
     request.session['comments'] = request.POST['comments']
-    return render(request,"survey/result.html")
+    return render(request,"survey/result.html",context)
+
+
+
+
+
+
+
+
+
+ 
+    
